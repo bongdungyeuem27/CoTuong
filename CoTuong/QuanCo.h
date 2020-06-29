dfb9ec4817c4ee2138db6fb76a8afc50c3829c8a
Khánh Lê Mai Duy <19521679@ms.uit.edu.vn>
2020-06-29 20:31:57 +0700

    2

--- CoTuong/QuanCo.h
+++ CoTuong/QuanCo.h
@@ -178,12 +178,12 @@ public:
     TinhDen()
     {
         p = Point(5, 1);
-        y1 = consox1, y2 = 5, x1 = consox1, x2 = consox2;
+        y1 = consoy1, y2 = 5, x1 = consox1, x2 = consox2;
     }
     TinhDen(int a, int b)
     {
         p = Point(a, b);
-        y1 = consox1, y2 = 5, x1 = consox1, x2 = consox2;
+        y1 = consoy1, y2 = 5, x1 = consox1, x2 = consox2;
     }
     void NuocDiCoBan()
     {
@@ -198,18 +198,18 @@ public:
         {
             for (int j = 0; j < den.size(); ++j)
             {
-                if ((trong[i].x + p.x == 2 * den[i]->Getpx()) && (trong[i].y + p.y == 2 * den[i]->Getpy()))
+                if ((trong[i].x + p.x == 2 * den[j]->Getpx()) && (trong[i].y + p.y == 2 * den[j]->Getpy()))
                 {
                     trong.erase(trong.begin() + i);
-                    --i;
+                    --j;
                 }
             }
             for (int j = 0; j < trang.size(); ++j)
             {
-                if ((trong[i].x + p.x == 2 * trang[i]->Getpx()) && (trong[i].y + p.y == 2 * trang[i]->Getpy()))
+                if ((trong[i].x + p.x == 2 * trang[j]->Getpx()) && (trong[i].y + p.y == 2 * trang[j]->Getpy()))
                 {
                     trong.erase(trong.begin() + i);
-                    --i;
+                    --j;
                 }
             }
         }
@@ -220,8 +220,8 @@ public:
         NuocDiCoBan();
         XoaChamTuong(trang, den);
         XoaChamDongDoi(trang, den);
-        XoaChamDoiPhuong(trang, den);
         XoaBiChan(trang, den);
+        XoaChamDoiPhuong(trang, den);       
         Menu(trang, den);
         trong.clear();
         cham.clear();
@@ -239,19 +239,113 @@ protected:
 public:
     MaDen()
     {
-        y1 = consox1, y2 = 3, x1 = 4, x2 = 6;
+        y1 = consoy1, y2 = consoy2, x1 = consox1, x2 = consox2;
     }
     MaDen(int a, int b)
     {
         p = Point(a, b);
-        y1 = consox1, y2 = 3, x1 = 4, x2 = 6;
+        y1 = consoy1, y2 = consoy2, x1 = consox1, x2 = consox2;
     }
     void NuocDiCoBan()
     {
-        trong.push_back(Point(p.x + 1, p.y + 1));
-        trong.push_back(Point(p.x - 1, p.y + 1));
-        trong.push_back(Point(p.x - 1, p.y - 1));
-        trong.push_back(Point(p.x + 1, p.y - 1));
+        trong.push_back(Point(p.x + 2, p.y + 1));
+        trong.push_back(Point(p.x - 2, p.y + 1));
+        trong.push_back(Point(p.x + 2, p.y - 1));
+        trong.push_back(Point(p.x - 2, p.y - 1));
+
+        trong.push_back(Point(p.x + 1, p.y + 2));
+        trong.push_back(Point(p.x + 1, p.y - 2));
+        trong.push_back(Point(p.x - 1, p.y + 2));
+        trong.push_back(Point(p.x - 1, p.y - 2));
+    }
+
+    void XoaBiChan(vector<QuanCo*>& trang, vector<QuanCo*>& den)
+    {
+        for (int i = 0; i < den.size(); ++i)
+        {
+            if (den[i]->Getpx() == p.x + 1 && den[i]->Getpy() == p.y)
+                for (int j = 0; j < trong.size(); ++j)
+                {
+                    if (trong[j].x == p.x + 2)
+                    {
+                        trong.erase(trong.begin() + j);
+                        --j;
+                    }
+                    
+                }
+            if (den[i]->Getpx() == p.x - 1 && den[i]->Getpy() == p.y)
+                for (int j = 0; j < trong.size(); ++j)
+                {
+                    if (trong[j].x == p.x - 2)
+                    {
+                        trong.erase(trong.begin() + j);
+                        --j;
+                    }
+
+                }
+            if (den[i]->Getpx() == p.x && den[i]->Getpy() == p.y + 1)
+                for (int j = 0; j < trong.size(); ++j)
+                {
+                    if (trong[j].y == p.y + 2)
+                    {
+                        trong.erase(trong.begin() + j);
+                        --j;
+                    }
+
+                }
+            if (den[i]->Getpx() == p.x && den[i]->Getpy() == p.y - 1)
+                for (int j = 0; j < trong.size(); ++j)
+                {
+                    if (trong[j].y == p.y - 2)
+                    {
+                        trong.erase(trong.begin() + j);
+                        --j;
+                    }
+                }
+        }
+
+        for (int i = 0; i < trang.size(); ++i)
+        {
+            if (trang[i]->Getpx() == p.x + 1 && trang[i]->Getpy() == p.y)
+                for (int j = 0; j < trong.size(); ++j)
+                {
+                    if (trong[j].x == p.x + 2)
+                    {
+                        trong.erase(trong.begin() + j);
+                        --j;
+                    }
+
+                }
+            if (trang[i]->Getpx() == p.x - 1 && trang[i]->Getpy() == p.y)
+                for (int j = 0; j < trong.size(); ++j)
+                {
+                    if (trong[j].x == p.x - 2)
+                    {
+                        trong.erase(trong.begin() + j);
+                        --j;
+                    }
+
+                }
+            if (trang[i]->Getpx() == p.x && trang[i]->Getpy() == p.y + 1)
+                for (int j = 0; j < trong.size(); ++j)
+                {
+                    if (trong[j].y == p.y + 2)
+                    {
+                        trong.erase(trong.begin() + j);
+                        --j;
+                    }
+
+                }
+            if (trang[i]->Getpx() == p.x && trang[i]->Getpy() == p.y - 1)
+                for (int j = 0; j < trong.size(); ++j)
+                {
+                    if (trong[j].y == p.y - 2)
+                    {
+                        trong.erase(trong.begin() + j);
+                        --j;
+                    }
+                }
+        }
     }
 
     void DiChuyen(vector<QuanCo*>& trang, vector<QuanCo*>& den)
@@ -259,12 +353,100 @@ public:
         NuocDiCoBan();
         XoaChamTuong(trang, den);
         XoaChamDongDoi(trang, den);
-        XoaChamDoiPhuong(trang, den);
+        XoaBiChan(trang, den);
+        XoaChamDoiPhuong(trang, den);      
         Menu(trang, den);
         trong.clear();
         cham.clear();
     }
     ~MaDen()
+    {
+    }
+};
+
+
+class XeDen : public Den
+{
+private:
+protected:
+public:
+    XeDen()
+    {
+        y1 = consoy1, y2 = consoy2, x1 = consox1, x2 = consox2;
+    }
+    XeDen(int a, int b)
+    {
+        p = Point(a, b);
+        y1 = consoy1, y2 = consoy2, x1 = consox1, x2 = consox2;
+    }
+    void NuocDiCoBan()
+    {
+        for (int i = p.x + 1; i <= x2; ++i)
+        {
+            trong.push_back(Point(i, p.y));
+        }
+
+        for (int i = p.x - 1; i >= x1; --i)
+        {
+            trong.push_back(Point(i, p.y));
+        }
+
+        for (int i = p.y + 1; i <= y2; ++i)
+        {
+            trong.push_back(Point(p.x, i));
+        }
+
+        for (int i = p.x - 1; i >= y1; --i)
+        {
+            trong.push_back(Point(p.x, i));
+        }        
+    }
+
+    void XoaBiChan(vector<QuanCo*>& trang, vector<QuanCo*>& den)
+    {
+        for (int i = 0; i < trong.size(); ++i)
+        {
+            for (int j = 0; j < den.size(); ++j)
+            {
+                if ((trong[i].x - p.x) > den[j]->Getpx() && p.y == den[j]->Getpy())
+                {
+                    trong.erase(trong.begin() + j);
+                    --j;
+                }
+
+                if ((p.x - trong[i].x) > den[j]->Getpx() && p.y == den[j]->Getpy())
+                {
+                    trong.erase(trong.begin() + j);
+                    --j;
+                }
+
+                if ((p.y - trong[i].y) > den[j]->Getpy() && p.x == den[j]->Getpx())
+                {
+                    trong.erase(trong.begin() + j);
+                    --j;
+                }
+
+                if ((trong[i].y - p.y) > den[j]->Getpy() && p.x == den[j]->Getpx())
+                {
+                    trong.erase(trong.begin() + j);
+                    --j;
+                }
+            }
+        }
+    }
+
+    void DiChuyen(vector<QuanCo*>& trang, vector<QuanCo*>& den)
+    {
+        NuocDiCoBan();
+        //XoaChamTuong(trang, den);
+        XoaBiChan(trang, den);
+        XoaChamDongDoi(trang, den);
+        XoaChamDoiPhuong(trang, den);
+        Menu(trang, den);
+        trong.clear();
+        cham.clear();
+    }
+    ~XeDen()
     {
     }
 };
